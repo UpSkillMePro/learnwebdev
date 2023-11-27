7d68bd59ace7cbb26b7f28010040a5f8ed4b3f12
Sandeep Patil <sandeep.spatil@gmail.com>
2023-11-27 14:09:43 +0100

    Added User API endpoints and configured drf-spectacular and added user authentication

--- learnwebdev/urls.py
+++ learnwebdev/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
+    path(
+        '',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('courses.urls')),
+    path('api/v1/user/', include('user.urls')),
 ]
